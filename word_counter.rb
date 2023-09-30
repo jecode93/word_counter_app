d5eac9c4017572c9fab54e717bbb0d6355bef41f
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-09-30 15:42:57 -0400

    Remove single quote from from text if they have.

--- word_counter.rb
+++ word_counter.rb
@@ -1,7 +1,7 @@
 text = "A man feeding a goat and a donkey. Now the goat becomes envious of the donkey, because he was too well fed. And she said to him: \"Between the millstone to turn and the burdens to carry, your life is an endless torment, \" and she advised him to simulate epilepsy, and to let himself fall into a hole to have rest . He followed the advice, dropped down and rubbed his whole body. His master having called the veterinarian, asked him for a remedy for the injured man. The veterinarian prescribed him to infuse the lung..."
 
 frequence = {}
-mots = text.tr('.,":', '').downcase.split(' ')
+mots = text.tr('.,":\'', '').downcase.split(' ')
 
 mots.each do |mot|
   if frequence[mot]
